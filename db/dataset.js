ee2bd293d1df774782078bd90314133a468f0272
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2025-08-13 23:57:33 -0500

    feat: :sparkles: añadiendo unos datos extras

--- db/dataset.js
+++ db/dataset.js
@@ -16,14 +16,18 @@ async function seedAll() {
 
     // Usuario 3 (Juan)
     { _id: 5, user_id: 3, nombre: "Arroz blanco", descripcion: "Cocinar arroz con agua y sal", ingredientes: ["Arroz", "Agua", "Sal"] },
-    { _id: 6, user_id: 3, nombre: "Pollo asado", descripcion: "Hornear pollo con especias", ingredientes: ["Pollo", "Pimienta", "Sal"] }
+    { _id: 6, user_id: 3, nombre: "Pollo asado", descripcion: "Hornear pollo con especias", ingredientes: ["Pollo", "Pimienta", "Sal"] },
+        { _id: 7, user_id: 4, nombre: "Té verde", descripcion: "Hervir agua y añadir té verde", ingredientes: ["Agua", "Té verde"] }
+
   
     ];    
 
     const usuarios =[
       { _id: 1, nombre: "Andres", cedula: 10245631, pais: "Colombia", id_recetas: [1, 2] },
     { _id: 2, nombre: "Maria", cedula: 20458963, pais: "Colombia", id_recetas: [3, 4] },
-    { _id: 3, nombre: "Juan", cedula: 30547896, pais: "Colombia", id_recetas: [5, 6] }
+    { _id: 3, nombre: "Juan", cedula: 30547896, pais: "Colombia", id_recetas: [5, 6] },
+        { _id: 4, nombre: "Luisa", cedula: 40658971, pais: "Colombia", id_recetas: [7] }
+
     ]
 
     await getDB().collection("usuarios").deleteMany();
